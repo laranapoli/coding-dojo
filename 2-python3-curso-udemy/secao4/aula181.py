d0fb6ca988c09fed57f121ae360d57f5660aa3d8
laranapoli <laranapoli0@gmail.com>
2025-03-24 19:59:58 -0300

    2025-03-24

---
+++ 2-python3-curso-udemy/secao4/aula181.py
@@ -0,0 +1,14 @@
+"""
+AMBIENTES VIRTUAIS NO PYTHON (venv)
+- Um ambiente virtual carrega toda a sua instalação
+do python para uma pasta no caminho escolhido.
+    > Carrega a versão global do python
+- Ao ativar um ambiente virtual, a instalação do 
+ambiente virtual será usada.
+- venv é o módulo que vamos usar para criar 
+ambientes virtuais.
+- Você pode dar o nome que preferir para um ambiente
+virtual, mas os mais comuns são: venv, env, .venv e .env 
+- Quando ativamos o ambiente virtual, ele muda temporariamente
+o PATH do python
+"""
